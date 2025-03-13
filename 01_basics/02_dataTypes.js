7ca3c6dc443ea76b6a3c97dcd47653f6b1d2eb6b
Akshat-Cipher010 <akshatnigam7931@gmail.com>
2025-03-13 16:47:42 +0530

    modified datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,36 @@
+"use strict";  //treat all Js code as newer version
+
+// alert(3 + 3); // we are using node.js not browser so alert is not available
+// console.log(3 + 3); // this will print 6 in the console
+
+let name = "Akshat"
+let age = 2
+let isMarried = false
+let state;
+
+//number => 2 to power 53
+//bigInt
+//string => ""
+//boolean => true/false
+//null => standalone value
+//undefined => variable is declared but not assigned any value
+//symbol => unique and immutable value
+//object => collection of key value pairs
+// let person = {
+//     name: "Akshat",
+//     age: 2,
+//     isMarried: false
+// }
+
+console.log(typeof(state));
+console.log(typeof "akshat");
+console.log(typeof 3.14);
+console.log(typeof true);
+console.log(typeof null);
+console.log(typeof undefined);
+// console.log(typeof person); // object
+// console.log(typeof {name: "Akshat", age: 2}); // object
+// console.log(typeof [1, 2, 3]); // object
+// console.log(typeof function(){}); // function
+// console.log(typeof Symbol("Akshat")); // symbol
+
