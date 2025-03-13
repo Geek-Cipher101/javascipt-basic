43603315672ba9559c9626e7a711c2a11214ddbe
Akshat-Cipher010 <akshatnigam7931@gmail.com>
2025-03-14 00:35:47 +0530

    made changes in comparision.js

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -13,20 +13,7 @@ console.log(booleanIsLoggedIn); // true
 let someNumber = 33
 let stringNumber = String(someNumber)
 console.log(stringNumber); // "33"
-console.log(typeof(stringNumber));
-
-// ****************************** Operations ****************************** //
-
-// 1. Arithmetic Operations
-
-
-
-
-
-
-
-
-
+console.log(typeof(stringNumber)); // string
 // '33' => 33
 // '33abc' => NaN (Not a Number)
 // true => 1
@@ -43,3 +30,31 @@ console.log(typeof(stringNumber));
 // 0 => false   ///telling for boolean operations
 // "" => false   ///telling for boolean operations
 // "abc" => true   ///telling for boolean operations
+
+
+// ****************************** Operations ****************************** //
+
+let value = 3
+let negValue = -value
+console.log(negValue);
+console.log(2**4); // 16
+
+let str1 = "hello";
+let str2 = " Akshat";
+console.log(str1 - str2); // NaN
+console.log(str1 + str2); // "hello Akshat"
+console.log("1" + 2); // "12"
+console.log(1 + "2"); // "12"
+console.log("1" + 2 + 3); // "123"
+console.log(1 + 2 + "3"); // "33"
+console.log(+true); // 1
+console.log(+false); // 0
+console.log(+""); // 0
+
+let num1 , num2 , num3
+num1 = num2 = num3 = 5 + 4
+console.log(num1, num2, num3); // 48 48 48
+
+let gamecounter = 100;
+gamecounter++;
+console.log(++gamecounter); // 102
