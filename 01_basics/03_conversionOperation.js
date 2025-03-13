c547cfd661b1a47e1aa4685529848d94324f7765
Akshat-Cipher010 <akshatnigam20@outlook.com>
2025-03-13 11:44:50 +0000

    conversion of datatypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,33 @@
+let score = "33abc"
+
+console.log(typeof(score));
+
+let valueInNumber = Number(score)
+console.log(typeof(valueInNumber));
+console.log(valueInNumber);// NaN
+
+let isLoggedIn = 1
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn); // true
+
+let someNumber = 33
+let stringNumber = String(someNumber)
+console.log(stringNumber); // "33"
+console.log(typeof(stringNumber));
+
+// '33' => 33
+// '33abc' => NaN (Not a Number)
+// true => 1
+// false => 0
+// null => 0
+// undefined => NaN
+// '' => 0
+// 'abc' => NaN
+// 'abc123' => NaN
+// '123abc' => NaN  
+// '123.45' => 123.45
+// '123.45.67' => NaN
+// 1 => true   ///telling for boolean operations
+// 0 => false   ///telling for boolean operations
+// "" => false   ///telling for boolean operations
+// "abc" => true   ///telling for boolean operations
